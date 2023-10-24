988e3487c33c0a41001a7795b5372baa5c3fc90a
L3gionaer <patrick@waidmann.email>
2023-10-24 19:38:55 +0200

    slideshow on the home page

---
+++ src/images/slideshow.ts
@@ -0,0 +1,3 @@
+export * as Gandalf from "./gandalf.jpg";
+export * as Blumen from "./blumen.jpg";
+export * as Mohn from "./mohn.jpg";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,78 @@
-import * as React from "react"
-import { FC } from "react";
+import * as React from "react";
+import { FC, useState, useRef, useEffect } from "react";
 import { Link } from "gatsby";
-import type { HeadFC } from "gatsby"
+import type { HeadFC } from "gatsby";
 
 import "./index.scss";
 import Layout from "../templates/layout/layout";
-import hero from "../images/hero.svg";
 import Button from "../components/button/button";
+import * as images from "../images/slideshow";
 
 const IndexPage: FC = () => {
+    const allImages = useRef(Object.values(images));
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            setCurrentImageIndex((index) => {
+                console.log("index", index);
+                if (index === allImages.current.length - 1) {
+                    return 0;
+                }
+                return index + 1;
+            });
+        }, 6000);
+    }, []);
+
     return (
         <Layout>
             <section className="heroSection">
                 <div className="heroContainer">
-                    <img className="heroImage" src={hero} />
                     <div className="heroContent">
-                        <h1>
-                            Rahmenlos
-                        </h1>
+                        <h1>Rahmenlos</h1>
                         <hr />
-                        <p>Die Auseinandersetzung mit dem gesehenem ist der Entstehungsweg ein jeden Bildes.</p>
-                        <p>Etwas auf sich wirken lassen und Schicht für Schicht für Schicht mit Farbe wiedergeben in seiner eigenen Interpretation.</p>
+                        <p>
+                            Die Auseinandersetzung mit dem gesehenem ist der
+                            Entstehungsweg ein jeden Bildes.
+                        </p>
+                        <p>
+                            Etwas auf sich wirken lassen und Schicht für Schicht
+                            für Schicht mit Farbe wiedergeben in seiner eigenen
+                            Interpretation.
+                        </p>
                         <Link to="/galery" className="link">
                             <Button>Zur Galerie</Button>
                         </Link>
                     </div>
+                    <div className="slideshowContainer">
+                        <div className="slideshow">
+                            {Object.entries(allImages.current).map(
+                                ([key, value]) => (
+                                    <div
+                                        className="slide"
+                                        style={{
+                                            transform: `translate(${
+                                                -currentImageIndex * 100
+                                            }%)`,
+                                        }}
+                                    >
+                                        <img
+                                            src={value.default}
+                                            className="heroImage"
+                                            alt="hero"
+                                            key={key}
+                                        />
+                                    </div>
+                                )
+                            )}
+                        </div>
+                    </div>
                 </div>
             </section>
         </Layout>
-    )
-}
+    );
+};
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Startseite</title>
+export const Head: HeadFC = () => <title>Startseite</title>;
